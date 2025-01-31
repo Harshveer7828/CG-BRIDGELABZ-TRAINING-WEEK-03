aff7c228824eb0dbb50fc2dd7268fa0fce068483
harshveer7828 <harshveer7828@gmail.com>
2025-01-31 13:37:35 +0530

    Day-03 InitialCommit

--- Day03/src/InsertionSort/InsertionSort.java
+++ Day03/src/InsertionSort/InsertionSort.java
@@ -1,4 +1,18 @@
 package InsertionSort;
 
 public class InsertionSort {
+
+    //Method to sort the insertion sort
+    static void insertionSort(int[] id){
+        int n = id.length;
+        for (int i = 1; i < n; i++) {
+            int key = id[i];
+            int j = i - 1;
+            while(j >= 0 && id[j] > key){
+                id[j + 1] = id[j];
+                j--;
+            }
+            id[j+1] = key;
+        }
+    }
 }
--- Day03/src/InsertionSort/Main.java
+++ Day03/src/InsertionSort/Main.java
@@ -1,4 +1,31 @@
 package InsertionSort;
 
+import BubbleSort.BubbleSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //Best time O(n) worst case O(n^2).
+        // For small dataset
+        Scanner input = new Scanner(System.in);
+        // Get the total students
+        System.out.println("Enter the total Employee :- ");
+        int n = input.nextInt();
+        int[] id = new int[n];
+        System.out.print("Enter the Employee ID :- ");
+        // Taking the user Input of student marks
+        for (int i = 0; i < id.length; i++) {
+            id[i] = input.nextInt();
+        }
+
+        // sort the array
+        InsertionSort.insertionSort(id);
+        System.out.print("Id's after sorting:- ");
+        System.out.print(Arrays.toString(id));
+
+
+
+    }
 }
