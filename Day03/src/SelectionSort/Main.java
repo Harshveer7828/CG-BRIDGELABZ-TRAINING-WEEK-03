f92663af9f2b38656919a0d32f3c0d7e152a7c77
harshveer7828 <harshveer7828@gmail.com>
2025-01-31 16:50:59 +0530

    Day-03 InitialCommit

--- Day03/src/SelectionSort/Main.java
+++ Day03/src/SelectionSort/Main.java
@@ -1,7 +1,26 @@
 package SelectionSort;
 
+import QuickSort.QuickSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Get the total Scores
+        System.out.println("Enter the number total Scores :- ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the Scores :- ");
+        // Taking the user Input of student marks
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
 
+        // sort the array
+        QuickSort.quickSort(arr,0,arr.length - 1);
+        System.out.print("Scores after sorting:- ");
+        System.out.print(Arrays.toString(arr));
     }
 }
