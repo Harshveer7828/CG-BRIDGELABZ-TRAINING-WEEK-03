9140e773bf86c60075dbc95acc77cf6438fce2fc
harshveer7828 <harshveer7828@gmail.com>
2025-02-03 21:26:52 +0530

    [Harsh veer singh] Add. Concatenate String array

---
+++ Day04/src/main/java/StringBuffer/ConcatenateStrings/ConcatenateString.java
@@ -0,0 +1,11 @@
+package StringBuffer.ConcatenateStrings;
+
+public class ConcatenateString {
+    public static String concatenate(String[] arr){
+        StringBuffer concatenatedString = new StringBuffer();
+        for (String s : arr) {
+            concatenatedString.append(s);
+        }
+        return concatenatedString.toString();
+    }
+}
---
+++ Day04/src/test/java/StringBuffer/ConcatenateStringTest.java
@@ -0,0 +1,29 @@
+package StringBuffer;
+import StringBuffer.ConcatenateStrings.ConcatenateString;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ConcatenateStringTest {
+    // Test One
+    @Test
+    void concatenateStringTest1(){
+        assertEquals("Harshveersinghthakur", ConcatenateString.concatenate(new String[]{"Harsh","veer","singh","thakur"}));
+    }
+
+    // Test second
+    @Test
+    void concatenateStringTest2(){
+        assertEquals("JhonDoe", ConcatenateString.concatenate(new String[]{"Jhon","Doe"}));
+    }
+
+    // Test third
+    @Test
+    void concatenateStringTest3(){
+        assertEquals("Omtiwari", ConcatenateString.concatenate(new String[]{"Om","tiwari"}));
+    }
+
+
+
+}
