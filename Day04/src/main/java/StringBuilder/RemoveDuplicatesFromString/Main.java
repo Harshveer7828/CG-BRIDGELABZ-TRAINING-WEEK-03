87246767465e494bfbc14661d528de13e76f00a2
harshveer7828 <harshveer7828@gmail.com>
2025-02-03 21:05:22 +0530

    [Harsh veer singh] Refactor. Changes Main class Add. Buffer reader for efficient input

--- Day04/src/main/java/StringBuilder/RemoveDuplicatesFromString/Main.java
+++ Day04/src/main/java/StringBuilder/RemoveDuplicatesFromString/Main.java
@@ -1,8 +1,25 @@
 package StringBuilder.RemoveDuplicatesFromString;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        String ans = RemoveDuplicates.deleteDuplicates("Harshveersingh");
-        System.out.println(ans);
+        // Try catch block to handle the io exception ny buffer reader
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            // Get user String
+            System.out.print("Enter the string :- ");
+            String userInput = br.readLine();
+
+            // Invoke the remover duplicate string method and display it
+            System.out.println(RemoveDuplicates.deleteDuplicates(userInput));
+        } catch (IOException e) {
+            // Caught the exception
+            System.out.println("Exception caught " + e.getMessage());
+        }
+
     }
 }
