771ab7e3c73c8e7761cb2ea0bafd2a6189598b4d
harshveer7828 <harshveer7828@gmail.com>
2025-02-05 11:17:33 +0530

    [Harsh veer singh] Refactor. Package name changes as per convention rule📦

---
+++ Day05/src/main/java/com/capgemini/binary_search/find_rotated_point/FindPoint.java
@@ -0,0 +1,5 @@
+package com.capgemini.binary_search.find_rotated_point;
+
+public class FindPoint {
+
+}
---
+++ Day05/src/main/java/com/capgemini/binary_search/find_rotated_point/Main.java
@@ -0,0 +1,8 @@
+package com.capgemini.binary_search.find_rotated_point;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "abc";
+
+    }
+}
