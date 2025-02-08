5779339441a1fd6ade995f44f8a5b6da559f1ebc
harshveer7828 <harshveer7828@gmail.com>
2025-02-08 15:51:57 +0530

    [Harsh veer singh] Add. Compare recursion and iterative fibonacchi test file added ✨

---
+++ Day06/src/main/java/com/capgemini/compare_recursion_and_iterative_fibonacchi/Main.java
@@ -0,0 +1,11 @@
+package com.capgemini.compare_recursion_and_iterative_fibonacchi;
+
+import com.capgemini.compare_string_concatenation_efficiency.DisplayTime;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculateTimeAndDisplay.displayTimeResult(10);
+        CalculateTimeAndDisplay.displayTimeResult(30);
+        CalculateTimeAndDisplay.displayTimeResult(50);
+    }
+}
